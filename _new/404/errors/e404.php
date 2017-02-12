d1cd0d1d7292f07ff4b2c51df9bf3641657fc406
Keith Chia <email@keithchia.com>
2017-02-12 18:35:30 +0800

    harp compiled

---
+++ _new/404/errors/e404.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-gb" lang="en-gb" dir="ltr">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width" />
+	<title>404 - Page not found</title>
+	<link rel="stylesheet" href="/css/error.css" type="text/css" />
+		</head>
+<body>
+
+	<div class="error-masthead">
+		<a href="http://www.nus.edu.sg"><img src="/images/logo-white.png" /></a>
+	</div>
+
+	<div class="error">
+		<h1 class="error-code">404</h1>
+		<h2 class="error-message">Oops, this page does not exist.</h2>
+		<div class="error-buttons">
+        	<!--link to your website's homepage-->
+			<a href="http://nus.edu.sg/">Return to the homepage</a>
+		</div>
+	</div>
+
+		
+</body>
+</html>
